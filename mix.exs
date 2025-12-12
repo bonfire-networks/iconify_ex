3be8e07a362fab373ee2ea20f6e639161af9ced1
Mayel de Borniol <git@mayel.space>
2025-12-12 00:38:59 +0000

    up

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Iconify.MixProject do
   use Mix.Project
 
   @project_url "https://github.com/bonfire-networks/iconify_ex"
-  @version "0.6.1"
+  @version "0.6.2"
 
   def project do
     [
